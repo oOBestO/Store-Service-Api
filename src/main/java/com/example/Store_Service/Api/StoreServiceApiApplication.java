8a673b65e53086d6511527f7482d163299edbe4f
oOBESTO <bestcung232@gmail.com>
2025-02-09 05:06:27 +0700

    first commit

---
+++ src/main/java/com/example/Store_Service/Api/StoreServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Store_Service.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Store_Service/Api/StoreServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Store_Service.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
